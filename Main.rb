7d1827308e73a9df1e430cb49b65a3ee1ff09f8e
Karabo Motsepe <memory.motsepe@gmail.com>
2022-05-03 16:15:13 -0700

    initial commit

---
+++ Main.rb
@@ -0,0 +1,4 @@
+require_relative 'require_all.rb'
+#--------------------------------#
+calculator = GpaCalculator.new
+puts calculator.calculate_gpa
---
+++ src/GradeProcessor.rb
@@ -0,0 +1,14 @@
+class GradeProcessor
+
+
+def return_first_part(letter_grade)
+		if !letter_grade.include?("stop") && letter_grade != nil
+		task = letter_grade.split('=>')
+		end
+
+	task
+
+end
+
+
+end 
